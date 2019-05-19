eca7b502ea92c8ebc32ff2d421df358436ef30ef
viktorhajer <hajer.viktor@gmail.com>
2019-05-19 18:21:52 +0200

    Stream gruard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './components/page-not-found.component';
+import {StreamGuard} from './shared/services/stream-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'devices', pathMatch: 'full'},
-  { path: 'inspect', loadChildren: './inspect/inspect.module#InspectModule' },
   { path: 'devices', loadChildren: './devices/devices.module#DevicesModule' },
-  { path: 'filters', loadChildren: './filters/filters.module#FiltersModule' },
-  { path: 'marker', loadChildren: './marker/marker.module#MarkerModule' },
-  { path: 'presenter', loadChildren: './presenter/presenter.module#PresenterModule' },
-  { path: 'detection', loadChildren: './detection/detection.module#DetectionModule' },
-  { path: 'mixer', loadChildren: './mixer/mixer.module#MixerModule' },
-  { path: 'color-keying', loadChildren: './color-keying/color-keying.module#ColorKeyingModule' },
-  { path: 'motion', loadChildren: './motion/motion.module#MotionModule' },
-  { path: 'tracking', loadChildren: './tracking/tracking.module#TrackingModule' },
+  { path: 'inspect', loadChildren: './inspect/inspect.module#InspectModule', canActivate: [StreamGuard] },
+  { path: 'filters', loadChildren: './filters/filters.module#FiltersModule', canActivate: [StreamGuard] },
+  { path: 'marker', loadChildren: './marker/marker.module#MarkerModule', canActivate: [StreamGuard] },
+  { path: 'presenter', loadChildren: './presenter/presenter.module#PresenterModule', canActivate: [StreamGuard] },
+  { path: 'detection', loadChildren: './detection/detection.module#DetectionModule', canActivate: [StreamGuard] },
+  { path: 'mixer', loadChildren: './mixer/mixer.module#MixerModule', canActivate: [StreamGuard] },
+  { path: 'color-keying', loadChildren: './color-keying/color-keying.module#ColorKeyingModule', canActivate: [StreamGuard] },
+  { path: 'motion', loadChildren: './motion/motion.module#MotionModule', canActivate: [StreamGuard] },
+  { path: 'tracking', loadChildren: './tracking/tracking.module#TrackingModule', canActivate: [StreamGuard] },
   { path: 'stereo', loadChildren: './stereo/stereo.module#StereoModule' },
   { path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/shared/services/stream-guard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { StreamService } from './stream.service';
+
+@Injectable({providedIn: 'root'})
+export class StreamGuard implements CanActivate {
+
+  constructor(private streamService: StreamService, private router: Router) {
+  }
+
+  canActivate(): boolean {
+    if (!this.streamService.streams.length){
+      this.router.navigate(['devices']);
+      return false;
+    }
+    return true;
+  }
+}
